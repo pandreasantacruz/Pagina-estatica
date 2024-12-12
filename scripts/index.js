c9a0190c342124022c3dd2447061b47fc6999006
pandreasantacruz <andreasantacruz123@gmail.com>
2024-12-12 13:11:02 -0600

    Proyecto completo

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,86 @@
-
 class Activity{
     constructor (id, title, description, imgUrl){
         this.id = id
         this.title = title
-        this.description =description
+        this.description = description
         this.imgUrl = imgUrl
     }
 }
 class Repository {
-    constructor (activities){
+    constructor (){
     this.activities = [];
+    this.id = 0
 }
 getAllActivities(){
     return this.activities;
 
 }
-createActivity(id, title, description, imgUrl){
-    let newActivity = new Activity(id, title, description, imgUrl);
+createActivity(title, description, imgUrl){
+    this.id++;
+    let newActivity = new Activity(this.id, title, description, imgUrl);
     this.activities.push(newActivity);
 }
-deleteActivity(id){
-    this.activities = this.activities.filter(activity => activity.id !== id);
 }
+// Crear una instancia del repositorio
+const activityRepository = new Repository();
+
+const botonRegistrar = document.getElementById("botonRegistrar")
+const tituloInput = document.getElementById("titulo");
+const descripcionInput = document.getElementById("descripcion");
+const urlimagenInput = document.getElementById("urlimagen");
+const contenedor = document.getElementById("contenedor")
+const botonBorrar = document.getElementById("botonBorrar")
+botonBorrar.style.display ="none"
+
+const handler = (e) => {
+    e.preventDefault(); 
+    
+    const titulo = tituloInput.value;
+    const descripcion = descripcionInput.value
+    const imagen = urlimagenInput.value
+
+    if (titulo === "" || descripcion === "" || imagen === ""  ){
+        alert ("Te faltan datos por ingresar")
+    } else { 
+
+        // Llamar al método createActivity y pasar los datos
+        activityRepository.createActivity(titulo, descripcion, imagen);
+
+        tituloInput.value = '';
+        descripcionInput.value = '';
+        urlimagenInput.value = '';
+
+    mostrarTarjetas();
+    
+    }
 }
+
+function mostrarTarjetas() {
+    
+    contenedor.innerHTML = ""
+    activityRepository.getAllActivities().map(activity => {
+        const tarjeta = document.createElement("div"); // Crear un nuevo div para cada tarjeta
+        tarjeta.classList.add("tarjeta");
+
+        tarjeta.innerHTML = `
+            <h3>Actividad Registrada:</h3>
+            <p>Título: ${activity.title}</p>
+            <p>Descripción: ${activity.description}</p>
+            <p>URL Imagen: <img src="${activity.imgUrl}" alt="Imagen Actividad" style="max-width: 100%; height: auto;" /></p>
+        `;
+        tarjeta.addEventListener("click", (event) => {
+            event.target.remove ()}) 
+
+        contenedor.appendChild(tarjeta); // Añadir la tarjeta al contenedor
+        botonBorrar.style.display ="flex"
+        botonBorrar.addEventListener("click", borrarTarjetas)
+    });
+    
+    }
+function borrarTarjetas (){
+    location.reload ()
+
+}
+    
+
+botonRegistrar.addEventListener("click", handler)
