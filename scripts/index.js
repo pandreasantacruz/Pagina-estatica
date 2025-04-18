3d3bd1105067b8e4b427901607dea91102b46fc1
pandreasantacruz <andreasantacruz123@gmail.com>
2025-04-18 17:58:19 -0500

    Resolver conflictos

--- scripts/index.js
+++ scripts/index.js
@@ -1,86 +1,80 @@
-class Activity{
-    constructor (id, title, description, imgUrl){
-        this.id = id
-        this.title = title
-        this.description = description
-        this.imgUrl = imgUrl
-    }
+class Activity {
+  constructor(id, title, description, imgUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.imgUrl = imgUrl;
+  }
 }
 class Repository {
-    constructor (){
+  constructor() {
     this.activities = [];
-    this.id = 0
-}
-getAllActivities(){
+    this.id = 0;
+  }
+  getAllActivities() {
     return this.activities;
-
-}
-createActivity(title, description, imgUrl){
+  }
+  createActivity(title, description, imgUrl) {
     this.id++;
     let newActivity = new Activity(this.id, title, description, imgUrl);
     this.activities.push(newActivity);
-}
+  }
 }
 // Crear una instancia del repositorio
 const activityRepository = new Repository();
 
-const botonRegistrar = document.getElementById("botonRegistrar")
+const botonRegistrar = document.getElementById("botonRegistrar");
 const tituloInput = document.getElementById("titulo");
 const descripcionInput = document.getElementById("descripcion");
 const urlimagenInput = document.getElementById("urlimagen");
-const contenedor = document.getElementById("contenedor")
-const botonBorrar = document.getElementById("botonBorrar")
-botonBorrar.style.display ="none"
+const contenedor = document.getElementById("contenedor");
+const botonBorrar = document.getElementById("botonBorrar");
+botonBorrar.style.display = "none";
 
 const handler = (e) => {
-    e.preventDefault(); 
-    
-    const titulo = tituloInput.value;
-    const descripcion = descripcionInput.value
-    const imagen = urlimagenInput.value
+  e.preventDefault();
 
-    if (titulo === "" || descripcion === "" || imagen === ""  ){
-        alert ("Te faltan datos por ingresar")
-    } else { 
+  const titulo = tituloInput.value;
+  const descripcion = descripcionInput.value;
+  const imagen = urlimagenInput.value;
 
-        // Llamar al método createActivity y pasar los datos
-        activityRepository.createActivity(titulo, descripcion, imagen);
+  if (titulo === "" || descripcion === "" || imagen === "") {
+    alert("Te faltan datos por ingresar");
+  } else {
+    // Llamar al método createActivity y pasar los datos
+    activityRepository.createActivity(titulo, descripcion, imagen);
 
-        tituloInput.value = '';
-        descripcionInput.value = '';
-        urlimagenInput.value = '';
+    tituloInput.value = "";
+    descripcionInput.value = "";
+    urlimagenInput.value = "";
 
     mostrarTarjetas();
-    
-    }
-}
+  }
+};
 
 function mostrarTarjetas() {
-    
-    contenedor.innerHTML = ""
-    activityRepository.getAllActivities().map(activity => {
-        const tarjeta = document.createElement("div"); // Crear un nuevo div para cada tarjeta
-        tarjeta.classList.add("tarjeta");
+  contenedor.innerHTML = "";
+  activityRepository.getAllActivities().map((activity) => {
+    const tarjeta = document.createElement("div"); // Crear un nuevo div para cada tarjeta
+    tarjeta.classList.add("tarjeta");
 
-        tarjeta.innerHTML = `
+    tarjeta.innerHTML = `
             <h3>Actividad Registrada:</h3>
             <p>Título: ${activity.title}</p>
             <p>Descripción: ${activity.description}</p>
             <p>URL Imagen: <img src="${activity.imgUrl}" alt="Imagen Actividad" style="max-width: 100%; height: auto;" /></p>
         `;
-        tarjeta.addEventListener("click", (event) => {
-            event.target.remove ()}) 
-
-        contenedor.appendChild(tarjeta); // Añadir la tarjeta al contenedor
-        botonBorrar.style.display ="flex"
-        botonBorrar.addEventListener("click", borrarTarjetas)
+    tarjeta.addEventListener("click", (event) => {
+      event.target.remove();
     });
-    
-    }
-function borrarTarjetas (){
-    location.reload ()
 
+    contenedor.appendChild(tarjeta); // Añadir la tarjeta al contenedor
+    botonBorrar.style.display = "flex";
+    botonBorrar.addEventListener("click", borrarTarjetas);
+  });
 }
-    
+function borrarTarjetas() {
+  location.reload();
+}
 
-botonRegistrar.addEventListener("click", handler)
+botonRegistrar.addEventListener("click", handler);
--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,5 +1,8 @@
 class Activity{
   constructor (id, title, description, imgUrl){
+      if (typeof id ===! "number") {
+        throw Error ("Id debe ser un numero")
+      }
       this.id = id
       this.title = title
       this.description = description
@@ -21,14 +24,42 @@ createActivity(title, description, imgUrl){
   this.activities.push(newActivity);
 }
 }
+describe("Se va a probar la clase Activity", function () {
+   
+    it ("Debe ser una clase", () => {
+    expect(typeof Activity.prototype.constructor).toBe("function");
+    });
+    
+    it ("Debe inicializar la Activity con los datos recibidos por parametros", () => {
+      const mockActivity = {
+        id: "1",
+        title: "TituloPrueba",
+        description: "Descripcion Prueba",
+        imgUrl:"ImagenPrueba"
+      };
+      const newActivity = new Activity(
+        mockActivity.id,
+        mockActivity.title,
+        mockActivity.description,
+        mockActivity.imgUrl
+      );
 
+      expect (newActivity.id).toBe(mockActivity.id),
+      expect(newActivity.title).toBe(mockActivity.title);
+      expect(newActivity.description).toBe(mockActivity.description);
+      expect(newActivity.imgUrl).toBe(mockActivity.imgUrl);
+
+    });  
+
+    it ("Debe ser numero", () => {
+        
+
+    })
+})
 describe("demo", function () {
   it("Este test debe pasar siempre", function () {
     expect(4 + 2).toBe(6);
   });
-  it("Debe ser una clase", () => {
-    expect(typeof Activity.prototype.constructor).toBe("function");
-});
 it("Debe ser una clase", () => {
     expect(typeof Repository.prototype.constructor).toBe("function");
 });
@@ -40,15 +71,5 @@ it("Debe get activities () retornar un array", () => {
     const activityRepository  =  new Repository ();
     expect(Array.isArray(activityRepository.getAllActivities ()).toBeTrue)
 });
-it("El metodo createActivity debe agregar actividades", () =>{
-    const activityRepository  =  new Repository ();
-    activityRepository.createActivity ('id: Prueba Id')
-    activityRepository.createActivity ('title: Prueba title')
-    activityRepository.createActivity ('description: Prueba description')
-    activityRepository.createActivity ('imgUrl: Prueba imgUrl');
-    expect(activityRepository.createActivity()).toEqual('id: Prueba Id');
-    expect(activityRepository.createActivity()).toEqual('title: Prueba title');
-    expect(activityRepository.createActivity()).toEqual('description: Prueba description');
-    expect(activityRepository.createActivity()).toEqual('imgUrl: Prueba imgUrl')
-});
+
 });
